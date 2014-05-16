aaca7ba8c7f128509148a3d1e37cafd1766e0980
ShuyinBlack <maha.chan.89@googlemail.com>
2014-05-16 15:13:56 +0200

    Rotate + Sprite Sheets
    SpriteSheets einfügen angefangen. Momentan für den Scrounge-Base-Button
    sowie für alle äußeren Hover-Buttons.
    Bisher nur für die höchste Auflösung umgesetzt. Muss für die zwei
    weiteren media queries Abstufungen noch angepasst werden.
    
    Gleichzeitig für die Hover-Buttons im äußeren Bereich auch rotate
    umgesetzt. Analog der SpriteSheets aber auch dies bisher nur für die
    höchste Auflösung.

--- client/client.js
+++ client/client.js
@@ -5,7 +5,195 @@
 if (Meteor.isClient){
 	//Subscriptions
   Meteor.subscribe("userData");
+
+
+/*Seltsame Darstellungsfehler bedürfen es, dass der Hintergrund um ein Pixel weiter verschoben wird, als die Datei es hergibt (beobachtet in Chrome)*/
+Template.standardBorder.events({
+
+/*Events Frame-Buttons + Hover*/
+
+    'mouseover #scrounge' : function(e, t){
+
+    	var pos = $('#scrounge').css("background-position");
+    	console.log(pos);
+    	$('#scrounge').css({"background-position":"0px -152px"});	
+    },
+
+    'mouseout #scrounge' : function(e, t){
+
+    	var pos = $('#scrounge').css("background-position");
+    	console.log(pos);
+    	$('#scrounge').css({"background-position":"0px 0px"});	
+    },
+
+
+/*HOVER*/
+
+    'mouseover #left_slider_category' : function(e, t){
+
+    	var pos = $('#left_slider_category').css("background-position");
+    	var size = $('#left_slider_category').css("padding");
+    	console.log(pos);
+    	console.log(size);
+
+    	/*Umsetzung der media queries in javascript, Abfrage über die Größe des Elements, muss noch für alle anderen Elemente übernommen werden*/
+    	switch (size) {
+
+    		case "110px 54px 0px 0px":
+
+    		$('#left_slider_category').css({"background-position":"-163px 0px"});
+
+    		break;
+
+    		case "":
+
+    		break;
+
+    		case"":
+
+    		break;
+
+    		default:
+
+    		console.log("something's wrong...");
+    	}    		
+    },
+
+    'mouseout #left_slider_category' : function(e, t){
+
+    	var pos = $('#left_slider_category').css("background-position");
+    	var size = $('#left_slider_category').css("padding");
+    	console.log(pos);
+
+    	switch (size) {
+
+    		case "110px 54px 0px 0px":
+
+    		$('#left_slider_category').css({"background-position":"-109px 0px"});
+
+    		break;
+
+    		case "":
+
+    		break;
+
+    		case"":
+
+    		break;
+
+    		default:
+
+    		console.log("something's wrong...");
+    	} 
+    		
+    },
+
+    'mouseover #right_slider_category' : function(e, t){
+
+    	var pos = $('#right_slider_category').css("background-position");
+    	console.log(pos);
+    	$('#right_slider_category').css({"background-position":"-163px 0px"});	
+    },
+
+    'mouseout #right_slider_category' : function(e, t){
+
+    	var pos = $('#right_slider_category').css("background-position");
+    	console.log(pos);
+    	$('#right_slider_category').css({"background-position":"-109px 0px"});	
+    },
+
+    'mouseover #up_slider_stolen' : function(e, t){
+
+    	var pos = $('#up_slider_stolen').css("background-position");
+    	console.log(pos);
+    	$('#up_slider_stolen').css({"background-position":"-55px 0px"});	
+    },
+
+    'mouseout #up_slider_stolen' : function(e, t){
+
+    	var pos = $('#up_slider_stolen').css("background-position");
+    	console.log(pos);
+    	$('#up_slider_stolen').css({"background-position":"0px 0px"});	
+    },
+
+    'mouseover #down_slider_stolen' : function(e, t){
+
+    	var pos = $('#down_slider_stolen').css("background-position");
+    	console.log(pos);
+    	$('#down_slider_stolen').css({"background-position":"-55px 0px"});	
+    },
+
+    'mouseout #down_slider_stolen' : function(e, t){
+
+    	var pos = $('#down_slider_stolen').css("background-position");
+    	console.log(pos);
+    	$('#down_slider_stolen').css({"background-position":"0px 0px"});	
+    },
+
+    'mouseover #down_slider_own' : function(e, t){
+
+    	var pos = $('#down_slider_own').css("background-position");
+    	console.log(pos);
+    	$('#down_slider_own').css({"background-position":"-55px 0px"});	
+    },
+
+    'mouseout #down_slider_own' : function(e, t){
+
+    	var pos = $('#down_slider_own').css("background-position");
+    	console.log(pos);
+    	$('#down_slider_own').css({"background-position":"0px 0px"});	
+    },
+
+    'mouseover #up_slider_own' : function(e, t){
+
+    	var pos = $('#up_slider_own').css("background-position");
+    	console.log(pos);
+    	$('#up_slider_own').css({"background-position":"-55px 0px"});	
+    },
+
+    'mouseout #up_slider_own' : function(e, t){
+
+    	var pos = $('#up_slider_own').css("background-position");
+    	console.log(pos);
+    	$('#up_slider_own').css({"background-position":"0px 0px"});	
+    },
+
+    'mouseover #right_slider_matter' : function(e, t){
+
+    	var pos = $('#right_slider_matter').css("background-position");
+    	console.log(pos);
+    	$('#right_slider_matter').css({"background-position":"-55px 0px"});	
+    },
+
+    'mouseout #right_slider_matter' : function(e, t){
+
+    	var pos = $('#right_slider_matter').css("background-position");
+    	console.log(pos);
+    	$('#right_slider_matter').css({"background-position":"0px 0px"});	
+    },
+
+    'mouseover #left_slider_matter' : function(e, t){
+
+    	var pos = $('#left_slider_matter').css("background-position");
+    	console.log(pos);
+    	$('#left_slider_matter').css({"background-position":"-55px 0px"});	
+    },
+
+    'mouseout #left_slider_matter' : function(e, t){
+
+    	var pos = $('#left_slider_matter').css("background-position");
+    	console.log(pos);
+    	$('#left_slider_matter').css({"background-position":"0px 0px"});	
+    }
+});
+
 }
+/*  function hoverScroungeBase() {
+	var pos = button.style.backgroundPosition;
+	alert(pos);*/
+	
+
+
 // if (Meteor.isClient) {
 //   //Methods
 //   Meteor.methods({
