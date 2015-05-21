91172e61f4c77d5f57832346542e3c28a4acd739
Julian Sakowski <Senju@hotmail.de>
2015-05-21 15:59:47 +0200

    small change

--- server/kadira.js
+++ server/kadira.js
@@ -1,5 +1,5 @@
 Meteor.startup(function() {
-	//local
+	//local Senju
     Kadira.connect('ixneWHgv2riiQbtvs', 'f1cc732d-449b-4cae-8c82-f2967713a050');
     // online
     // Kadira.connect('2SrQhZ5bkZvWfcQXS', '30fd0e3c-b3f9-4246-8c76-b29936bed52a')
